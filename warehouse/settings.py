9dc29d0316788bbfd1a1816ef333df596080e50f
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2021-11-26 04:08:07 +0100

    updated settings..

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'warehouse',
-        'USER': 'postgres',
-        'PASSWORD': 'olatunbosun',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'PORT': 5432,
-        'HOST': 'localhost'
+        'HOST': ''
     }
 }
 
